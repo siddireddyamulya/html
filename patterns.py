673992290863041c0b550b4dbc24b5cf95b787d4
Amulyasiddireddy <siddireddyamulya9@gmail.com>
2025-08-08 17:46:16 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,156 @@
+
+# 7 6 5 4 3 2 1
+# 6 5 4 3 2 1
+# 5 4 3 2 1
+# 4 3 2 1
+# 3 2 1
+# 2 1
+# 1
+
+# for i in range(7, 0, -1):
+#     for j in range(i, 0, -1):
+#        print(j, end=" ")
+#     print()
+
+
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i, end="")
+#     print()
+
+
+
+# 11111
+# 2222
+# 333
+# 44
+# 5
+
+# for i in range(1,6):
+#     for j in range(i,6):
+#         print(i, end="")
+#     print()
+
+
+
+# *
+# **
+# ***
+# ****
+# *****
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print("*", end="")
+#     print()
+
+
+
+
+# 1 
+# 1 2 
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+# n=5
+# for i in range(1,n+1):
+#     for j in range(n-1):
+#         print(" ",end=" ")
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()    
+
+
+# 1 0 0 0 0 
+# 0 1 0 0 0 
+# 0 0 1 0 0
+# 0 0 0 1 0
+# 0 0 0 0 1
+
+# n=5
+# for i in range(n):
+#     for j in range(n):
+#         if i==j:
+#             print("1",end=" ")
+#         else:
+#             print("0",end=" ")    
+#     print()
+    
+
+
+
+# 6 6 6 6 6 6 
+# 5 5 5 5 5 
+# 4 4 4 4 
+# 3 3 3 
+# 2 2 
+# 1 
+
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print()
+
+
+
+
+# * * * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+# for i in range(6,0,-1):
+#     for j in range(i):
+#         print("*", end=" ")
+#     print()
+
+
+
+
+
+
+
+
+
+n=4
+for i in range(n):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
